ef41bb47d50bb097bad72182d477ec58c4ce3774
mitchellciupak <ciupak@purdue.edu>
2020-10-11 17:42:08 -0400

    Development Complete

--- docScan.py
+++ docScan.py
@@ -31,7 +31,7 @@ def detectContours(img):
 
     contours, hierarchy = cv2.findContours(img,mode=cv2.RETR_EXTERNAL,method=cv2.CHAIN_APPROX_NONE)
     for cnt in contours:
-        area = cv2.contourArea(contours)
+        area = cv2.contourArea(cnt)
         if area > 5000: #reduce noise and add drawing
             cv2.drawContours(img_contour, cnt, contourIdx=-1, color=(255, 0, 0), thickness=3)
             peri = cv2.arcLength(curve=cnt,closed=True)
@@ -43,6 +43,32 @@ def detectContours(img):
                 maxArea = area
     return bestContour
 
+def orderWarp(pts):
+    pts = pts.reshape((4,2))
+    ptsUpdated = np.zeros((4,1,2),np.int32)
+
+    add = pts.sum(1)
+    diff = np.diff(pts, axis=1)
+
+    ptsUpdated[0] = pts[np.argmin(add)]
+    ptsUpdated[1] = pts[np.argmin(diff)]
+    ptsUpdated[2] = pts[np.argmax(diff)]
+    ptsUpdated[3] = pts[np.argmax(add)]
+
+    return ptsUpdated
+
+def detectWarp(img,contour):
+    contour = orderWarp(contour)
+
+    pts1 = np.float32(contour)
+    pts2 = np.float32([[0, 0], [WIDTH_IMG, 0], [0, HEIGHT_IMG], [WIDTH_IMG, HEIGHT_IMG]])
+
+    matrix = cv2.getPerspectiveTransform(pts1, pts2)
+    imgOut = cv2.warpPerspective(img, matrix, (WIDTH_IMG, HEIGHT_IMG))
+
+    return cv2.resize(imgOut,(WIDTH_IMG,HEIGHT_IMG))
+
+
 while True:
     isValid, img = cap.read()
     img = cv2.resize(img,(WIDTH_IMG,HEIGHT_IMG))
@@ -50,9 +76,9 @@ while True:
 
     img_thresh = preProcessing(img)
     bestContour = detectContours(img_thresh)
-    getWarp(img,bestContour)
+    img_warped = detectWarp(img,bestContour)
 
-    cv2.imshow("Output", img_contour)
+    cv2.imshow("Output", img_warped)
 
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
